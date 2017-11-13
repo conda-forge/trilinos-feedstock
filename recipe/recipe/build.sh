e55cda625030fe283576c0d661e6d419801eda0f
Bill Spotz <wfspotz@sandia.gov>
2017-11-13 12:04:42 -0700

    Clean up build.sh
    Removed SEACAS enable, because there is a
    CMAKE_Fortran_LINK_EXECUTABLE problem Removed all unused CMake
    options.

--- recipe/recipe/build.sh
+++ recipe/recipe/build.sh
@@ -18,7 +18,6 @@ cmake \
   -D TPL_ENABLE_MPI:BOOL=ON \
   -D MPI_BASE_DIR:PATH=$PREFIX \
   -D MPI_EXEC:FILEPATH=$PREFIX/bin/mpiexec \
-  -D MPI_EXEC_PRE_NUMPROCS_FLAGS:STRING="$MPI_FLAGS" \
   -D PYTHON_EXECUTABLE:FILEPATH=$PYTHON \
   -D Trilinos_ENABLE_Fortran:BOOL=OFF \
   -D Trilinos_ENABLE_ALL_PACKAGES:BOOL=OFF \
@@ -52,7 +51,7 @@ cmake \
   -D Trilinos_ENABLE_Belos:BOOL=ON \
   -D Trilinos_ENABLE_ShyLU:BOOL=ON \
   -D Trilinos_ENABLE_Amesos2:BOOL=ON \
-  -D Trilinos_ENABLE_SEACAS:BOOL=ON \
+  -D Trilinos_ENABLE_SEACAS:BOOL=OFF \
   -D Trilinos_ENABLE_Komplex:BOOL=ON \
   -D Trilinos_ENABLE_Anasazi:BOOL=ON \
   -D Trilinos_ENABLE_Ifpack2:BOOL=ON \
@@ -61,18 +60,15 @@ cmake \
   -D Trilinos_ENABLE_FEI:BOOL=ON \
   -D Trilinos_ENABLE_Teko:BOOL=ON \
   -D Trilinos_ENABLE_Intrepid:BOOL=ON \
-  -D Trilinos_ENABLE_Intrepid2:BOOL=ON \
   -D Trilinos_ENABLE_STK:BOOL=OFF \
   -D Trilinos_ENABLE_Phalanx:BOOL=ON \
   -D Trilinos_ENABLE_NOX:BOOL=ON \
   -D NOX_ENABLE_LOCA:BOOL=ON \
   -D Trilinos_ENABLE_MueLu:BOOL=ON \
   -D Trilinos_ENABLE_Rythmos:BOOL=ON \
-  -D Trilinos_ENABLE_Tempus:BOOL=ON \
   -D Trilinos_ENABLE_Stokhos:BOOL=ON \
   -D Trilinos_ENABLE_ROL:BOOL=ON \
   -D Trilinos_ENABLE_Piro:BOOL=ON \
-  -D Trilinos_ENABLE_Panzer:BOOL=ON \
   -D Trilinos_ENABLE_TrilinosCouplings:BOOL=ON \
   -D Trilinos_ENABLE_Pike:BOOL=ON \
   $SRC_DIR

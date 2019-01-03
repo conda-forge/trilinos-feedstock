7d81fb5a93e6c857684ccb1f39789be67e4c0332
Julien Schueller <schueller@phimeca.com>
2019-01-03 13:32:26 +0100

    Select packages

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,8 +5,13 @@ cmake -LAH -G"NMake Makefiles" ^
     -DCMAKE_BUILD_TYPE=Release ^
     -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%"  ^
     -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
+    -DTrilinos_ENABLE_Fortran:BOOL=OFF ^
+    -DTrilinos_ENABLE_ALL_PACKAGES:BOOL=OFF ^
+    -DTrilinos_ENABLE_ALL_OPTIONAL_PACKAGES:BOOL=OFF ^
     -DTrilinos_ENABLE_TESTS:BOOL=OFF ^
     -DTrilinos_ENABLE_EXAMPLES:BOOL=OFF ^
+    -DTrilinos_ENABLE_Teuchos:BOOL=ON ^
+    -DTrilinos_ENABLE_ROL:BOOL=ON ^
     ..
 if errorlevel 1 exit 1
 
